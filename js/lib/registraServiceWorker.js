1fae7dad31e06676473c9f5442b44e25d7da9624
Hector Cordova <ganttasticos@gmail.com>
2026-01-26 18:45:04 -0600

    VERSION 1.06

--- js/lib/registraServiceWorker.js
+++ js/lib/registraServiceWorker.js
@@ -1,12 +1,15 @@
-const nombreDeServiceWorker = "sw.js"
+// js/lib/main.js
+
+// Registro del Service Worker
+const nombreDeServiceWorker = "sw.js";
 
 try {
- navigator.serviceWorker.register(nombreDeServiceWorker)
-  .then(registro => {
-   console.log(nombreDeServiceWorker, "registrado.")
-   console.log(registro)
-  })
-  .catch(error => console.log(error))
+  navigator.serviceWorker.register(nombreDeServiceWorker)
+    .then(registro => {
+      console.log(nombreDeServiceWorker, "registrado.");
+      console.log(registro);
+    })
+    .catch(error => console.log(error));
 } catch (error) {
- console.log(error)
+  console.log(error);
 }
--- sw.js
+++ sw.js
@@ -15,7 +15,7 @@
  * conoce como número menor y se cambia cuando se realizan
  * modificaciones menores.
  */
-const VERSION = "1.05"
+const VERSION = "1.06"
 
 /** Nombre del archivo de cache. */
 const CACHE = "ejemploPWA"
