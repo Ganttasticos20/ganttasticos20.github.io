1ddcabe18fdde54c1c17084c2492baa88abea5a4
Hector Cordova <ganttasticos@gmail.com>
2026-01-26 20:16:16 -0600

    VERSION 1.10

--- sw.js
+++ sw.js
@@ -15,7 +15,7 @@
  * conoce como número menor y se cambia cuando se realizan
  * modificaciones menores.
  */
-const VERSION = "1.09"
+const VERSION = "1.10"
 
 /** Nombre del archivo de cache. */
 const CACHE = "ejemploPWA"
