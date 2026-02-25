a00c33658bc8c6b724eb4c839d5e537c0811d7b8
Hector Cordova <ganttasticos@gmail.com>
2026-02-25 02:53:38 -0600

    version final 1.85

--- sw.js
+++ sw.js
@@ -1,23 +1,49 @@
-/* Archivo Service Worker - Ganttásticos (VERSIÓN FINAL) */
-const VERSION = "4.0"; 
-const CACHE = "Ganttasticos-v4.0"; 
+/* Archivo Service Worker - Ganttásticos (Versión Actualizada con Maskables) */
+const VERSION = "1.78"; 
+const CACHE = "Ganttasticos-v1.78"; // Actualizamos la versión de caché también
 
 const ARCHIVOS = [
-  "./",
   "index.html",
-  "manifest.json", 
+  "site.webmanifest",
   "css/estilos.css",
   "img/LOGO.png",
+  "img/LOGO2.png",
   "img/LOGO12.png",
+  "img/LOGO92.png",
+  "img/analisis.png",
+  "img/BALTA.png",
+  "img/gyn.png",
+  "img/FRENTEGANT.png",
+  "img/HECTOR.png",
+  "img/ITATI.png",
+  "img/iu.png",
+  "img/MENDIETA.png",
+  "img/PSICO.png",
+  "img/psicologos.png",
+  "img/pwa.png",
+  "img/ROBER.png",
+  "img/sync.png",
+  "img/Vanne.png",
+  "img/web.png",
+  "img/Movil.png",
+  "img/Escritorio.png",
+  "img/oficina.png",
+  "img/maskable_icon_x48.png",
+  "img/maskable_icon_x72.png",
+  "img/maskable_icon_x96.png",
+  "img/maskable_icon_x128.png",
   "img/maskable_icon_x192.png",
+  "img/maskable_icon_x384.png",
   "img/maskable_icon_x512.png",
-  "js/lib/registraServiceWorker.js"
+  "img/maskable_icon.png",
+  "js/lib/registraServiceWorker.js",
+  "./"
 ];
 
 self.addEventListener("install", (evt) => {
   evt.waitUntil(
     caches.open(CACHE).then((cache) => {
-      // Usamos map y catch para que si un archivo falla, no detenga todo
+      // Usamos Promise.allSettled para que si falta una imagen, el SW se instale de todos modos
       return Promise.allSettled(
         ARCHIVOS.map(url => cache.add(url))
       ).then(() => self.skipWaiting());
