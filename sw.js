a789b4b403382e09736bcf58c4e0e4357fb16745
Hector Cordova <ganttasticos@gmail.com>
2026-02-25 02:24:05 -0600

    l

--- sw.js
+++ sw.js
@@ -1,16 +1,38 @@
-const CACHE_NAME = "v10";
-const ASSETS = ["./", "index.html", "examen.json", "css/estilos.css"];
+const CACHE_NAME = "gantt-v20";
+// Agrega solo lo básico aquí para que no falle
+const ASSETS = [
+  "./",
+  "index.html",
+  "manifest.json",
+  "css/estilos.css",
+  "img/LOGO12.png",
+  "img/maskable_icon_x192.png",
+  "img/maskable_icon_x512.png"
+];
 
 self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      // Esto evita que el error 'Request failed' detenga todo
+      return Promise.allSettled(
+        ASSETS.map(url => cache.add(url).catch(err => console.log("Falló: " + url)))
+      );
+    })
+  );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (e) => {
-  e.waitUntil(caches.keys().then((ks) => Promise.all(ks.map((k) => k !== CACHE_NAME && caches.delete(k)))));
-  self.clients.claim();
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.map((k) => k !== CACHE_NAME && caches.delete(k))
+    ))
+  );
+  return self.clients.claim();
 });
 
 self.addEventListener("fetch", (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
 });
