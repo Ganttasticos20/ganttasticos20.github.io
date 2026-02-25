41bd0c5fccc362e7598b59b32393b684b820ef80
Hector Cordova <ganttasticos@gmail.com>
2026-02-25 02:28:49 -0600

    lisñslsk

--- sw.js
+++ sw.js
@@ -1,10 +1,16 @@
-const CACHE_NAME = "gantt-v20";
-// Agrega solo lo básico aquí para que no falle
+const CACHE_NAME = "gantt-v30";
+// Solo los archivos que SI existen y son necesarios
 const ASSETS = [
   "./",
   "index.html",
   "manifest.json",
   "css/estilos.css",
+  "img/BALTA.png",
+  "img/HECTOR.png",
+  "img/ITATI.png",
+  "img/MENDIETA.png",
+  "img/ROBER.png",
+  "img/Vanne.png",
   "img/LOGO12.png",
   "img/maskable_icon_x192.png",
   "img/maskable_icon_x512.png"
@@ -13,9 +19,9 @@ const ASSETS = [
 self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      // Esto evita que el error 'Request failed' detenga todo
+      // Usamos map y catch para que si un archivo falta, no se trabe el resto
       return Promise.allSettled(
-        ASSETS.map(url => cache.add(url).catch(err => console.log("Falló: " + url)))
+        ASSETS.map(url => cache.add(url).catch(err => console.log("Falta: " + url)))
       );
     })
   );
@@ -23,16 +29,10 @@ self.addEventListener("install", (e) => {
 });
 
 self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => Promise.all(
-      keys.map((k) => k !== CACHE_NAME && caches.delete(k))
-    ))
-  );
-  return self.clients.claim();
+  e.waitUntil(caches.keys().then((ks) => Promise.all(ks.map((k) => k !== CACHE_NAME && caches.delete(k)))));
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
